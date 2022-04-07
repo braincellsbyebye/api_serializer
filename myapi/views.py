7a01159c7ecf45da066be2255a27c9c85a212659
JC <ezjcd101@gmail.com>
2022-04-07 14:27:07 +0800

    created api

--- myapi/views.py
+++ myapi/views.py
@@ -1,3 +1,10 @@
+from django.http import QueryDict
 from django.shortcuts import render
+from rest_framework import viewsets
+from . serializers import vhcseriliazer
+from . models import vehicles
 
+class vehicleviewset(viewsets.ModelViewSet):
+    query = vehicles.objects.all().order_by('name')
+    serializer_class = vhcseriliazer
 # Create your views here.
